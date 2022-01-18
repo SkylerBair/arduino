35eb6b397f69f4bb72ff5f9483b33e7af5b7bb80
@skylerbair <skylertimothybair@hotmail.com>
2022-01-17 22:49:27 -0700

    fixed blinky to resisted version

--- main.go
+++ main.go
@@ -5,67 +5,20 @@ import (
 	"time"
 )
 
-/*
-motor1 = AFMotor //motor 1 is the drive motor
-motor2 = AFMotor //motor 2 is the turning servo motor
-var val int
-bt = serial.read
-motor1.setSpeed(255)
-motor2.setspeed(225)
-serial.begin(9600)
-*/
 func main() {
 
-	machine.InitADC()
-	ldr := machine.ADC{machine.ADC0}
-	ldr.Configure()
-
-	led := machine.Pin(machine.D9)
+	led := machine.D13
 	led.Configure(machine.PinConfig{Mode: machine.PinOutput})
+	ledSwitch := true
 
 	for {
-
-		print(ldr.Get())
-
-		if ldr.Get() > 40000 {
-			led.Set(true)
-		} else {
-			led.Set(false)
-		}
-
-		time.Sleep(time.Millisecond * 100)
+		led.Set(ledSwitch)
+		ledSwitch = !ledSwitch
+		delay(500)
 	}
 
 }
 
-/*
-if bt == "1" {
-	motor1 = forward
+func delay(t uint32) {
+	time.Sleep(time.Duration(1000000 * t))
 }
-
-func forward() int {
-	motor1.run(forward)
-	return
-}
-
-func movebackward() string {
-	motor1 := 2
-	return
-}
-
-func turnleft() string {
-	motor2 := 3
-	return
-}
-
-func turnright() string {
-	motor2 := 4
-	return
-}
-
-func stopcar() string {
-	motor1 := 0
-	return
-
-}
-*/
