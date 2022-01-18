da9b04fd9b8f66a3493778e3512c9c5975263939
@skylerbair <skylertimothybair@hotmail.com>
2022-01-17 22:38:01 -0700

    created Blinky

--- main.go
+++ main.go
@@ -16,13 +16,38 @@ serial.begin(9600)
 */
 func main() {
 
-	led := machine.LED
-	led.Configure(machine.PinConfig{mode: machine.PinOutput})
+	delay := func(t uint16) {
+		time.Sleep(time.Duration(1000000 * uint32(t)))
+	}
+
+	leds := []machine.Pin{
+		machine.D2,
+		machine.D3,
+		machine.D4,
+		machine.D5,
+		machine.D6,
+		machine.D7,
+	}
+
+	for _, led := range leds {
+		led.Configure(machine.PinConfig{Mode: machine.PinOutput})
+	}
+
+	index, delta := 0, 1
+
 	for {
-		led.High()
-		time.sleep(time.Millisecond * 500)
-		led.Low()
-		time.sleep(time.Millisecond * 500)
+
+		for i, led := range leds {
+			led.Set(i == index)
+		}
+
+		index += delta
+
+		if index == 0 || index == len(leds)-1 {
+			delta *= -1
+		}
+
+		delay(75)
 	}
 }
 
